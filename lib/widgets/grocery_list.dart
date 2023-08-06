cbf690362809f8e29e6fcead83eb9f7399f82784
Osha Prima Adidaya <adidayaosha@gmail.com>
2023-08-06 18:45:29 +0700

    added Get Function from Firebase, and loading state when adding new items

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
+import 'package:shopping_list/data/categories.dart';
+
+import 'package:http/http.dart' as http;
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -10,8 +15,49 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+// if you already have type anotation, you don't have to set var again.
+  List<GroceryItem> _groceryItems = [];
+  var _isLoading = true;
 
+// INITIALIZE
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+// GET ITEM
+  void _loadItems() async {
+    final url = Uri.https('flutter-project-791a9-default-rtdb.asia-southeast1.firebasedatabase.app', 'shopping-list.json');
+    final response = await http.get(url);
+
+    // decode first and then loop the data.
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> loadedItems = [];
+    for (final item in listData.entries) {
+      // first where: find the first matching item that you found.
+      final category = categories.entries
+          .firstWhere(
+            (catItem) => catItem.value.title == item.value['category'],
+          )
+          .value;
+
+      loadedItems.add(
+        GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        ),
+      );
+    }
+    setState(() {
+      _groceryItems = loadedItems;
+      _isLoading = false;
+    });
+  }
+
+// ADD ITEM
   void _addItem() async {
     final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
@@ -22,11 +68,13 @@ class _GroceryListState extends State<GroceryList> {
     if (newItem == null) {
       return;
     }
+
     setState(() {
       _groceryItems.add(newItem);
     });
   }
 
+// REMOVE ITEM
   void _removeItem(GroceryItem item) {
     setState(() {
       _groceryItems.remove(item);
@@ -39,6 +87,12 @@ class _GroceryListState extends State<GroceryList> {
     Widget content = const Center(
       child: Text('No items added yet'),
     );
+
+    if (_isLoading) {
+      content = const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
 
     if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -6,6 +6,7 @@ import 'package:shopping_list/models/category.dart';
 
 // HTTP
 import 'package:http/http.dart' as http;
+import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -23,14 +24,16 @@ class _NewItemState extends State<NewItem> {
   var _enteredName = '';
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
+  var _isSending = false;
 
   void _saveItem() async {
     // reach all form field, and execute the validation with validate()
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      final url = Uri.https(
-          'flutter-project-791a9-default-rtdb.asia-southeast1.firebasedatabase.app',
-          'shopping-list.json');
+      setState(() {
+        _isSending = true;
+      });
+      final url = Uri.https('flutter-project-791a9-default-rtdb.asia-southeast1.firebasedatabase.app', 'shopping-list.json');
       final res = await http.post(
         url,
         headers: {
@@ -46,6 +49,8 @@ class _NewItemState extends State<NewItem> {
         ),
       );
 
+      final Map<String, dynamic> resData = json.decode(res.body);
+
       print(res.body);
       // Pop: return to previous screen
       // Navigator.of(context).pop(
@@ -58,7 +63,14 @@ class _NewItemState extends State<NewItem> {
         return;
       }
 
-      Navigator.of(context).pop();
+      Navigator.of(context).pop(
+        GroceryItem(
+          id: resData['name'],
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory,
+        ),
+      );
     }
   }
 
@@ -80,10 +92,7 @@ class _NewItemState extends State<NewItem> {
                   label: Text('Name'),
                 ),
                 validator: (value) {
-                  if (value == null ||
-                      value.isEmpty ||
-                      value.trim().length <= 1 ||
-                      value.trim().length > 50) {
+                  if (value == null || value.isEmpty || value.trim().length <= 1 || value.trim().length > 50) {
                     return 'Must be between 1 and 50 characters.';
                   }
                   return null;
@@ -106,10 +115,7 @@ class _NewItemState extends State<NewItem> {
                       keyboardType: TextInputType.number,
                       initialValue: _enteredQuantity.toString(),
                       validator: (value) {
-                        if (value == null ||
-                            value.isEmpty ||
-                            int.tryParse(value) == null ||
-                            int.tryParse(value)! <= 0) {
+                        if (value == null || value.isEmpty || int.tryParse(value) == null || int.tryParse(value)! <= 0) {
                           return 'Must be a valid, positive number.';
                         }
                         return null;
@@ -163,14 +169,25 @@ class _NewItemState extends State<NewItem> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: () {
-                      _formKey.currentState!.reset();
-                    },
+                    // in Flutter, function are just values.
+                    // to disable button, just add null.
+                    onPressed: _isSending
+                        ? null
+                        : () {
+                            _formKey.currentState!.reset();
+                          },
                     child: const Text('Reset'),
                   ),
                   ElevatedButton(
-                    onPressed: _saveItem,
-                    child: const Text('Add Item'),
+                    onPressed: _isSending ? null : _saveItem,
+                    // create loading state to the button.
+                    child: _isSending
+                        ? const SizedBox(
+                            height: 16,
+                            width: 16,
+                            child: CircularProgressIndicator(),
+                          )
+                        : const Text('Add Item'),
                   )
                 ],
               )
